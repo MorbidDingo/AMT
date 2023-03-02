7d5f2911ca37c399ce196de6bf6decdee03dd920
MorbidDingo <111993770+MorbidDingo@users.noreply.github.com>
2023-03-02 20:22:14 +0530

    Minor changes
    Please check

--- login/login_form.php
+++ login/login_form.php
@@ -15,22 +15,12 @@ if(isset($_POST['submit'])){
 
     $result=mysqli_query($conn,$select);
 
-    if(mysqli_num_rows($result)>0){
-        $row=mysqli_fetch_array($result);
-        if($row['user_type']== 'admin'){
-            $_SESSION['admin_name'] = $row['name'];
-            header('location:admin_page.php');
-
-        }elseif($row['user_type']== 'user'){
-
-            //$_SESSION['user_name'] = $row['name'];
-            $_SESSION['user_id'] = $row['id'];
-            $id = $_SESSION['user_id'];
-            $_SESSION['user_name'] = $row['name'];
-            //echo($id);
-            header('location:../home/index.php');            
-        }
-    }else{
+    if($result)
+    {
+      header("Location:../home/index.php");
+    }
+    
+    else{
         $error[]='incorrect email or password!!';
     }
 };
